04a39081af18556a6d6768d502e34eb1df5a16cf
FelemixX <83292474+FelemixX@users.noreply.github.com>
2022-12-24 23:48:36 +0300

    [feature/1_step] - footer, header, connections added. gitignore adjusted

---
+++ template/header.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+    <title>CRUD</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-nav ml-auto nav-flex-icons d-md-flex align-items-center w-100">
+    <div class="fluid-container">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item me-5">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item me-5">
+                <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item me-5">
+                <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item me-5">
+                <a class="nav-link" href="#">About</a>
+            </li>
+        </ul>
+    </div>
+</nav>
