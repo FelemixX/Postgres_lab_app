805c153168e093144ca5373ae22c81fcfcfeec7f
FelemixX <83292474+FelemixX@users.noreply.github.com>
2022-12-25 00:13:52 +0300

    [feature/1_step] - auth form added

--- index.php
+++ index.php
@@ -1,3 +1,48 @@
 <?php require_once 'template/header.php'?>
-
+    <div class="container">
+        <div class="d-flex justify-content-center">
+            <div class="d-xl-inline-flex p-5 bd-highlight col-lg-4">
+                <div class="col-12">
+                    <h1 class="text-center">
+                        Авторизация
+                    </h1>
+                    <form method="post" action="/auth/login.php">
+                        <div class="mb-3">
+                            <label for="user_login text-center" class="form-label">
+                                Логин
+                            </label>
+                            <input name="user_login" required type="text" class="form-control" id="user_login"
+                                   aria-describedby="user login" placeholder="admin">
+                        </div>
+                        <div class=" mb-3">
+                            <label for="user_password" class="form-label">
+                                Пароль
+                            </label>
+                            <input name="user_password" required type="password" class="form-control" id="user_password" placeholder="theMostPowerfulPassword1337">
+                        </div>
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-dark mr-3">
+                                Войти
+                            </button>
+                            <a type="submit" class="d-inline-block btn btn-secondary" href="/auth/register_page.php">
+                                Регистрация
+                            </a>
+                        </div>
+                    </form>
+                    <?php if (isset($_GET["error"])): ?>
+                        <div class="alert alert-danger d-flex align-items-center alert-dismissible fade show mt-3"
+                             role="alert">
+                            <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:">
+                                <use xlink:href="#exclamation-triangle-fill"/>
+                            </svg>
+                            <div>
+                                Ошибка! Пользователя не существует!
+                            </div>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php require_once 'template/footer.php'?>
